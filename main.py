79f13a26c88df0e6e3000fa35926c0b1c8c4a213
Sandhya Devi <sandhuu822@gmail.com>
2026-06-04 21:57:20 +0530

    Added first FastAPI application

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World-FastAPI Basics"}
+
+@app.get("/about")
+def about():
+    return {"message":"This is About Page"}
+
+@app.get("/contact")
+def contact():
+    return {"message":"Contact"}
+
+@app.get("/student")
+def student():
+    return {
+        "name":"Sandhuu",
+        "Semester":5
+    }
